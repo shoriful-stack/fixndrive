84aa9c18854167a9e1b05f7739f4706de8441d00
Md. Shoriful Islam <sisazid182713@gmail.com>
2025-06-16 10:45:27 +0600

    get service by id endpoint created

--- src/components/Cards/ServiceCard.jsx
+++ src/components/Cards/ServiceCard.jsx
@@ -1,15 +1,15 @@
+import Link from "next/link";
 import React from "react";
 import { FaArrowRight } from "react-icons/fa6";
 
 const ServiceCard = ({ service }) => {
-  const { title, img, price } = service || {};
+  const { title, img, price, _id } = service || {};
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-sm mx-auto">
       <div
         className="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md"
         style={{
-          backgroundImage:
-            `url(${img})`,
+          backgroundImage: `url(${img})`,
         }}
       />
       <div className="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
@@ -20,9 +20,11 @@ const ServiceCard = ({ service }) => {
           <span className="font-bold text-[#FF3811] dark:text-gray-200">
             Price: {price}
           </span>
-          <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-[#FF3811] rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none cursor-pointer">
-            <FaArrowRight />
-          </button>
+          <Link href={`/services/${_id}`}>
+            <button className="px-2 py-1 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-[#FF3811] rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none cursor-pointer">
+              <FaArrowRight />
+            </button>
+          </Link>
         </div>
       </div>
     </div>
